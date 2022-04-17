90fbc6e0fe453d5714d12fdc9ddeedd7c0f54804
vanesa <vanemanza1@gmail.com>
2022-04-17 00:44:43 -0300

    agregué el id al metodo str del mod Dpto

--- ProyectoPrincipal/departamentos/models.py
+++ ProyectoPrincipal/departamentos/models.py
@@ -2,8 +2,8 @@ from django.db import models
 
 # Create your models here.
 class Departamento(models.Model):
-    nombre = models.CharField(max_length=50)
+    nombre = models.CharField('Nombre', max_length=50) # el primer parametro es como figurará en el admin de django
     codigo = models.IntegerField()
 
     def __str__(self):
-        return self.nombre
+        return f' {self.id} {self.nombre}'
