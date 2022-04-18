fe83bb2b19190a962c1eaa82fc3eb38822089020
vanesa <vanemanza1@gmail.com>
2022-04-17 22:57:54 -0300

    custum admin

--- ProyectoPrincipal/personas/admin.py
+++ ProyectoPrincipal/personas/admin.py
@@ -1,6 +1,31 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Persona
+from .models import Persona, Habilidades
 
-admin.site.register(Persona)
+admin.site.register(Habilidades)
+
+class EmpleadoAdmin(admin.ModelAdmin):
+    list_display =[
+        'nombre',
+        'apellido',
+        'puesto',
+        'departamento',
+        'nombre_completo'
+    ]
+
+    def nombre_completo(self, obj):
+        return f'{obj.nombre} {obj.apellido}'
+
+    #TODO 
+    # agregar una funcion q me calcule la edad a partir de la fecha de nacimiento
+    # agregar la fecha de nacimiento en el modelo   
+    # 
+    # TODO
+    # VER SI ES NECESARIO INSTALAR CKEDITOR ? 
+
+    search_fields = ('nombre', 'apellido')
+    list_filter = ('departamento', 'habilidad')
+    filter_horizontal = ('habilidad',)
+
+admin.site.register(Persona, EmpleadoAdmin)
--- ProyectoPrincipal/personas/models.py
+++ ProyectoPrincipal/personas/models.py
@@ -1,12 +1,25 @@
-from django.db import models
 from departamentos.models import Departamento
+from django.db import models
 
 # Create your models here.
+
+class Habilidades(models.Model):
+    habilidad = models.CharField('Habilidad', max_length=50)
+
+    class meta:
+        verbose_name = 'Habilidad'
+        verbose_name_plural = 'Habilidades'
+
+    def __str__(self):
+        return self.habilidad
+
+
 class Persona(models.Model):
     nombre = models.CharField(max_length=100)
     apellido = models.CharField(max_length=100)
     puesto = models.CharField(max_length=100)
     departamento = models.ForeignKey(Departamento, on_delete=models.CASCADE)
+    habilidad = models.ManyToManyField(Habilidades)
 
     def __str__(self) -> str:
         return f'{self.nombre} {self.apellido}'
