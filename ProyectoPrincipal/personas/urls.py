b9ee4373badf092b5add40df3ada710ee6485fed
vanesa <vanemanza1@gmail.com>
2022-04-24 17:42:06 -0300

    lista por palabras y habilidades

--- ProyectoPrincipal/personas/urls.py
+++ ProyectoPrincipal/personas/urls.py
@@ -4,5 +4,7 @@ from .views import *
 
 urlpatterns = [
      path('lista_empleados/', EmpleadosListView.as_view(), name='lista_empleados' ),
-     path('lista_por_area/<departamento>', EmpleadosPorAreaListView.as_view(), name='lista_por_area' )
+     path('lista_por_area/<departamento>', EmpleadosPorAreaListView.as_view(), name='lista_por_area' ),
+     path('lista_por_palabra', EmpleadosPorKword.as_view(), name='lista_por_kword' ),
+     path('habilidades', HabilidadesList.as_view(), name='habilidades'),
 ]
