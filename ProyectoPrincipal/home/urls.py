6a0260b4c956c4914af0ea322fa15bb443edce10
vanemanza1 <vanesa@infra-ThinkPad-E470>
2022-05-21 20:53:37 -0300

    corrijo nombre de la url

--- ProyectoPrincipal/home/urls.py
+++ ProyectoPrincipal/home/urls.py
@@ -5,5 +5,5 @@ urlpatterns = [
     #path('', Prueba.as_view(), name='prueba'),
     path('lista/', PruebaListView.as_view(), name='prueba_lista'),
     path('lista-prueba/', ListarPruebaListView.as_view(), name='lista'),
-     path('add/', PruebaCreateView.as_view(), name='add'),
+     path('agregar/', PruebaCreateView.as_view(), name='agregar'),
 ]
