9a99a765674feed5a846733c2b5ac35f63bc1e03
vanemanza1 <vanesa@infra-ThinkPad-E470>
2022-05-25 23:12:36 -0300

    agrego codigo null para modificar la tabla

--- ProyectoPrincipal/departamentos/models.py
+++ ProyectoPrincipal/departamentos/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 class Departamento(models.Model):
     nombre = models.CharField('Nombre', max_length=50) # el primer parametro es como figurará en el admin de django
-    codigo = models.IntegerField()
+    codigo = models.IntegerField(null=True)
 
     def __str__(self):
         return f' {self.id} {self.nombre}'
