d1d03fa226bf9ead4117e2529d267ba9005a9485
vanemanza1 <vanesa@infra-ThinkPad-E470>
2022-05-25 23:17:31 -0300

    reverse_lazy en PruebaCreateView

--- ProyectoPrincipal/home/views.py
+++ ProyectoPrincipal/home/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.urls import reverse_lazy
 from django.views.generic import TemplateView, ListView, CreateView
 from .models import Prueba
 from .forms import PruebaForm
@@ -29,5 +30,5 @@ class PruebaCreateView(CreateView):
     model = Prueba
     template_name = "agregar.html"
     form_class = PruebaForm
-    success_url = '/'
+    success_url = reverse_lazy('prueba_lista')
     
