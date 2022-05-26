94950751d2796a0b495a43e384c576e0f5af4084
vanemanza1 <vanesa@infra-ThinkPad-E470>
2022-05-25 23:13:27 -0300

    probando vistas genericas

--- ProyectoPrincipal/departamentos/urls.py
+++ ProyectoPrincipal/departamentos/urls.py
@@ -1,6 +1,14 @@
 from django.urls import path
-from .views import departamento
+#from .views import departamento
+from .views import NuevoDepartamentoView, DepartamentosView
+#from django.views.generic import TemplateView
+
+# urlpatterns = [   
+#     path('', departamento, name='departamentos'),
+# ]
 
 urlpatterns = [   
-    path('', departamento, name='departamentos'),
+    path('', DepartamentosView.as_view(), name='inicio'),
+    path('nuevo/', NuevoDepartamentoView.as_view(), name='crear_departamento'),
 ]
+
