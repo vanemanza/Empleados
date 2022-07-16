afa1fed17a3e2358342db6b5607c2db83da53be9
vanemanza1 <vanesa@infra-ThinkPad-E470>
2022-07-15 21:27:20 -0300

    campo area al mod dpto

--- ProyectoPrincipal/departamentos/models.py
+++ ProyectoPrincipal/departamentos/models.py
@@ -4,6 +4,7 @@ from django.db import models
 class Departamento(models.Model):
     nombre = models.CharField('Nombre', max_length=50) # el primer parametro es como figurará en el admin de django
     codigo = models.IntegerField(null=True)
+    area = models.CharField('Area', max_length=50, null=True, blank=True)
 
     def __str__(self):
         return f' {self.id} {self.nombre}'
