03d44f6a0a23462c473452c4cee20e0540717fa9
vanemanza1 <vanesa@infra-ThinkPad-E470>
2022-07-15 22:48:30 -0300

    agrego filtros al admin de dpto

--- ProyectoPrincipal/departamentos/admin.py
+++ ProyectoPrincipal/departamentos/admin.py
@@ -2,4 +2,12 @@ from django.contrib import admin
 from .models import Departamento
 # Register your models here.
 
-admin.site.register(Departamento)
+
+class DepartamentoAdmin(admin.ModelAdmin):
+    list_display = ['nombre', 'codigo', 'area']
+
+    search_fields = ('nombre', 'codigo', 'area' )
+    list_filter = ('nombre', 'codigo', 'area')
+ 
+admin.site.register(Departamento, DepartamentoAdmin)
+
